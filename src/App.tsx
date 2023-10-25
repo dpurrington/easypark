a112b8a55d30192d53cdd665f1400ae7630c4fb4
Dave Purrington <dave.purrington@gmail.com>
2023-10-25 15:19:24 -0400

    adding database connection

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,13 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import DataGridDemo from "./ListSessions";
+import SessionsList from "./SessionsList";
+import { app } from "./firebase"
 
 function App() {
   return (
     <div className="App">
-      <DataGridDemo />
+      <SessionsList />
     </div>
   );
 }
